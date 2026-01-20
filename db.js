a23bcf256198e9e7b2a866009ad06ba2b4bab98f
Siva6383 <sivaharish638349@gmail.com>
2026-01-21 04:56:50 +0530

    initial Commmited

--- db.js
+++ db.js
@@ -3,14 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = await mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
-
+const db = await mysql.createPool(process.env.MYSQL_URL);
 
 console.log("✅ MySQL Connected Successfully");
 
