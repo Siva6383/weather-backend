9203aa58611e55cc0216d66505c8ea92c571f8bb
Siva6383 <sivaharish638349@gmail.com>
2026-01-21 04:50:02 +0530

    Initial commitment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "https://weather-frontend-nine-blush.vercel.app/",
+  origin: "https://weather-frontend-nine-blush.vercel.app",
   credentials: true
 }));
 app.use(express.json());
