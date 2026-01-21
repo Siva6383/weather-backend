b5e6b7b720c95de433f71ff73b87fe649f410010
Siva6383 <sivaharish638349@gmail.com>
2026-01-21 17:12:02 +0530

    Initial Commit

--- db.js
+++ db.js
@@ -3,6 +3,27 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool(process.env.MYSQL_URL);
+let pool;
+
+if (process.env.MYSQL_URL) {
+  // ☁ Railway MySQL
+  console.log("☁ Using Railway MySQL");
+
+  pool = mysql.createPool(process.env.MYSQL_URL);
+
+} else {
+  // 💻 Local MySQL
+  console.log("💻 Using Local MySQL");
+
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10
+  });
+}
 
 export default pool;
