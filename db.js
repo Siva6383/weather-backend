66ce619ddecdf61d852e46a7272cb296e711514e
Siva6383 <sivaharish638349@gmail.com>
2026-01-21 16:47:44 +0530

    Initial Commit

--- db.js
+++ db.js
@@ -1,22 +1,8 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-if (!process.env.MYSQL_PUBLIC_URL && !process.env.DB_PASSWORD) {
-  throw new Error("❌ No database credentials found");
-}
+dotenv.config();
 
-let pool;
-
-if (process.env.MYSQL_PUBLIC_URL) {
-  console.log("☁ Using Railway MySQL");
-  pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
-} else {
-  console.log("💻 Using Local MySQL");
-  pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: process.env.DB_PASSWORD,
-    database: "weather_detection",
-  });
-}
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
 export default pool;
